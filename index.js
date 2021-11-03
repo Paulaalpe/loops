6353c7148d2a6d40259f8ba2d35002d06e02ff3d
Paulaalpe <91777530+Paulaalpe@users.noreply.github.com>
2021-11-03 20:01:07 +0100

    Iteration#3

--- index.js
+++ index.js
@@ -11,19 +11,27 @@
 
 /*Iteración #2: Condicionales avanzados    Comprueba en cada uno de los usuarios que tenga al menos dos trimestres aprobados y añade la propiedad isApproved a true o false en consecuencia. Una vez lo tengas compruébalo con un console.log.*/
 
-const alumns = [
-        {name: 'Pepe Viruela', T1: false, T2: false, T3: true}, 
-		{name: 'Lucia Aranda', T1: true, T2: false, T3: true},
-		{name: 'Juan Miranda', T1: false, T2: true, T3: true},
-		{name: 'Alfredo Blanco', T1: false, T2: false, T3: false},
-		{name: 'Raquel Benito', T1: true, T2: true, T3: true}
-]
+// const alumns = [
+//         {name: 'Pepe Viruela', T1: false, T2: false, T3: true}, 
+// 		{name: 'Lucia Aranda', T1: true, T2: false, T3: true},
+// 		{name: 'Juan Miranda', T1: false, T2: true, T3: true},
+// 		{name: 'Alfredo Blanco', T1: false, T2: false, T3: false},
+// 		{name: 'Raquel Benito', T1: true, T2: true, T3: true}
+// ]
 
-function alumnosAprobados(param) {
-    for (i=0; i<param.length;i++){
-        if ((param[i].T1 === true && param[i].T2 === true) || (param[i].T2 === true && param[i].T3 === true)||(param[i].T1 === true && param[i].T3 === true) ){
-            param[i].isApproved = true;
-            console.log(param[i])
-        } 
-    };
-};
+// function alumnosAprobados(param) {
+//     for (i=0; i<param.length;i++){
+//         if ((param[i].T1 === true && param[i].T2 === true) || (param[i].T2 === true && param[i].T3 === true)||(param[i].T1 === true && param[i].T3 === true) ){
+//             param[i].isApproved = true;
+//             console.log(param[i])
+//         } 
+//     };
+// };
+
+
+/* Iteración #3: Probando For...of      Usa un bucle forof para recorrer todos los destinos del array. Imprime en un console.log sus valores.}*/
+
+const placesToTravel = ['Japon', 'Venecia', 'Murcia', 'Santander', 'Filipinas', 'Madagascar']
+
+for (let place of placesToTravel) {
+    console.log(place)};
