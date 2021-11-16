87131e26cac59117e246068d187e63692f629d45
Paulaalpe <91777530+Paulaalpe@users.noreply.github.com>
2021-11-16 22:50:32 +0100

    Iteration#8

--- index.js
+++ index.js
@@ -72,19 +72,36 @@
 
 // Iteración #6: Mixed For...of e includes
 // Usa un bucle for...of para recorrer todos los juguetes y elimina los que incluyan la palabra gato. Recuerda que puedes usar la función .includes() para comprobarlo.Puedes usar este array:
-const toys = [
-    {id: 5, name: 'Buzz MyYear'}, 
-    {id: 11, name: 'Action Woman'}, 
-    {id: 23, name: 'Barbie Man'}, 
-    {id: 40, name: 'El gato con Guantes'},
-    {id: 40, name: 'El gato felix'}
-    ]
+// const toys = [
+//     {id: 5, name: 'Buzz MyYear'}, 
+//     {id: 11, name: 'Action Woman'}, 
+//     {id: 23, name: 'Barbie Man'}, 
+//     {id: 40, name: 'El gato con Guantes'},
+//     {id: 40, name: 'El gato felix'}
+//     ]
 
-let juguetesSinGato = [];
+// let juguetesSinGato = [];
+// for (toy of toys) {
+//     if (toy.name.includes('gato')) {
+//         continue;
+//     }
+//     juguetesSinGato.push(toy)
+// };
+// console.log(juguetesSinGato)
+
+// Iteración #7: For...of avanzado
+// Usa un bucle for...of para recorrer todos los juguetes y añade los que tengan más de 15 ventas (sellCount) al array popularToys. Imprimelo por consola
+const popularToys = [];
+const toys = [
+	{id: 5, name: 'Buzz MyYear', sellCount: 10}, 
+	{id: 11, name: 'Action Woman', sellCount: 24}, 
+	{id: 23, name: 'Barbie Man', sellCount: 15}, 
+	{id: 40, name: 'El gato con Guantes', sellCount: 8},
+	{id: 40, name: 'El gato felix', sellCount: 35}
+]
 for (toy of toys) {
-    if (toy.name.includes('gato')) {
-        continue;
+    if (toy.sellCount > 15){
+        popularToys.push(toy);
     }
-    juguetesSinGato.push(toy)
-};
-console.log(juguetesSinGato)
+}
+console.log(popularToys)
