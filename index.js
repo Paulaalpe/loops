ef48d4f0968884c6e0eac8b17268f85b71a03a3d
Paulaalpe <91777530+Paulaalpe@users.noreply.github.com>
2021-11-16 22:21:44 +0100

    Iteration#5

--- index.js
+++ index.js
@@ -12,7 +12,7 @@
 /*Iteración #2: Condicionales avanzados    Comprueba en cada uno de los usuarios que tenga al menos dos trimestres aprobados y añade la propiedad isApproved a true o false en consecuencia. Una vez lo tengas compruébalo con un console.log.*/
 
 // const alumns = [
-//         {name: 'Pepe Viruela', T1: false, T2: false, T3: true}, 
+//      {name: 'Pepe Viruela', T1: false, T2: false, T3: true}, 
 // 		{name: 'Lucia Aranda', T1: true, T2: false, T3: true},
 // 		{name: 'Juan Miranda', T1: false, T2: true, T3: true},
 // 		{name: 'Alfredo Blanco', T1: false, T2: false, T3: false},
@@ -39,13 +39,33 @@
 
 /* Iteración #4  Iteración #4: Probando For...in        Usa un for...in para imprimir por consola los datos del alienígena. */
 
-const alien = {
-    name: 'Wormuck',
-    race: 'Cucusumusu',
-    planet: 'Eden',
-    weight: '259kg'
-}
+// const alien = {
+//     name: 'Wormuck',
+//     race: 'Cucusumusu',
+//     planet: 'Eden',
+//     weight: '259kg'
+// }
 
-for (let clave in alien) {
-    console.log(alien[clave]); 
+// for (let clave in alien) {
+//     console.log(alien[clave]); 
+// }
+
+// Iteración #5: Probando For
+// Usa un bucle for para recorrer todos los destinos del array y elimina los elementos que tengan el id 11 y 40. Imprime en un console log el array. 
+const placesToTravel = [
+    {id: 5, name: 'Japan'}, 
+    {id: 11, name: 'Venecia'}, 
+    {id: 23, name: 'Murcia'}, 
+    {id: 40, name: 'Santander'}, 
+    {id: 44, name: 'Filipinas'}, 
+    {id: 59, name: 'Madagascar'}
+]
+
+let placesFinales = [];
+for(place of placesToTravel) {
+    if (place.id !== 11) {
+        if (place.id !==5) {
+        placesFinales.push(place);
+    }}
 }
+console.log(placesFinales)
